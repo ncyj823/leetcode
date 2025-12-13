5db56a63c98476ee7eb111f790e0847cfd1d5070
ncyj823 <ncy1504@gmail.com>
2025-12-14 00:17:50 +0530

    Update PROBLEM121 solution

--- PROBLEM121.cpp
+++ PROBLEM121.cpp
@@ -1,16 +1,21 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int mini = prices[0];
-        int maxprofit =0;
-        int n = prices.size();
-        for(int i = 0 ; i<n ; i++)
-        {
-            int cost = prices[i]-mini;
-            maxprofit= max(maxprofit , cost);
-            mini= min(mini,prices[i]);
-        }
-        return maxprofit;
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> maxProfit(vector<int>& prices) {
+    int mini = prices[0];
+    int maxprofit =0;
+    int n = prices.size();
+    for(int i = 0 ; i<n ; i++)
+    {
+        int cost = prices[i]-mini;
+        maxprofit= max(maxprofit , cost);
+        mini= min(mini,prices[i]);
     }
-};
-
+    return {maxprofit};
+}
+int main(){
+    vector<int> prices = {7,1,5,3,6,4};
+    vector<int> result = maxProfit(prices);
+    cout << "Maximum Profit: " << result[0] << endl;
+    return 0;
+}
